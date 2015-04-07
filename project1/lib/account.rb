0f656f5a19f7b0b4c5564a4922a6297af463fb03
Adam Bray <adam.bray@yale.edu>
2015-04-06 20:36:51 -0400

    added comments

--- project1/lib/account.rb
+++ project1/lib/account.rb
@@ -6,4 +6,14 @@ class Account < ActiveRecord::Base
    return "#{id} #{name}"
  end
 
+ # this is the balance method that we wrote in our one-on-one.
+ def balance
+   # we can call `transactions` in the lines below, because an acct has_many
+   # transactions, and this method is called on specific (instances) of 'Acconut'
+   # doing so will return just the transactions associated with the account
+   # that we're calling 'balance' on.
+   deposit_sum = transactions.where(category: "deposit").sum(:amount)
+   withdrawal_sum = transactions.where(category: "withdraw").sum(:amount)
+   return depost_sum - withdrawal_sum
+ end
 end
--- project1/lib/transaction.rb
+++ project1/lib/transaction.rb
@@ -2,6 +2,7 @@ class Transaction < ActiveRecord::Base
   belongs_to :account
   validates :name,    presence: true
   validates :amount,  presence: true
+  # good validations and use of `to_s` !
   def to_s
     return "id: #{id} {name: #{name}, amount: #{amount}, category: #{category}}"
   end
